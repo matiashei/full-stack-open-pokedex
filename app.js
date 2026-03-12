edc19c1b424a36513bf70391e84d920e31449184
Matias Heino <matias.heino@hotmail.com>
2026-03-12 13:39:51 +0200

    Refactor broken deployment simulation once more...

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.status(500).send('error')
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
